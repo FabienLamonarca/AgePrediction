d7ede1d2d790482922850a57a81a6ee54c7a68dc
FabienLamonarca <fabien.lamonarca@gmail.com>
2017-01-05 14:02:30 +0100

    END

--- src/plsr/plsr.m
+++ src/plsr/plsr.m
@@ -1,12 +1,12 @@
 % plsr(faces, 'LBP', {k1,k2,k3,k4,k5})
 
 function MAE = plsr( bdd, feature, folds )
+
 %% INIT
 profile on
 sections = [22 37 68];
 numFolds = length( folds );
-
-
+MAE = 0;
 
 %% arrays of input and label vector for each fold
 kX = cell( numFolds, 1 );
@@ -34,6 +34,7 @@ end
 
 
 %% FOREACH FOLD
+C = cell(numFolds, 3);
 for kk = 1:numFolds
     X = [];
     Y = [];
@@ -52,10 +53,10 @@ for kk = 1:numFolds
     end
     
     %% DEBUG 
-    assignin('base', 'X', X);
-    assignin('base', 'Y', Y);
-    assignin('base', 'T', T);
-    assignin('base', 'TY', TY);
+    %assignin('base', 'X', X);
+    %assignin('base', 'Y', Y);
+    %assignin('base', 'T', T);
+    %assignin('base', 'TY', TY);
     
     %% LEARNING THE GLOBAL PLSR ON THE OVERALL TRAINING SET
     [kXL,kYL,kXS,kYS,BETAS_GLOBAL] = plsregress( X, Y, 30 );
@@ -69,22 +70,46 @@ for kk = 1:numFolds
         [kXL,kYL,kXS,kYS,BETAS_LOCAL] = plsregress( LOCAL_X, LOCAL_Y, 30 );
         S{i,5} = BETAS_LOCAL;
     end
-    assignin('base', 'S', S);
+    % assignin('base', 'S', S);
  
     %% DOING A PREDICTION BASED ON THE GLOBAL REGRESSOR
-    reg_overall = regress(T, BETAS_GLOBAL);
+    reg_overall = round(regress(T, BETAS_GLOBAL));
+    %assignin('base', 'reg_overall', reg_overall);
     
-    %% SORTING reg IN SECTIONS DEFINED ABOVE
-    %%%%%%%% on fait des groupes de features sur lequel on
-    %%%%%%%% appliquera les regresseurs locaux
+    %% SORTING reg IN SECTIONS DEFINED ABOVE BASED ON PREDICTED AGE
+    S2 = split(T, reg_overall, sections, TY);
     
     %% USING LOCAL REGRESSOR ON SECTIONS
     % on parcourt les tranches, et on utilise regress()
-    profile viewer
-    return; % On reste sur le premier testing set
+    for i = 1 : size(S2,1)
+        S2{i,5} = regress(S2{i,3}, S{i,5});
+    end
+    % assignin('base', 'S2', S2);
+    
+    %% RETRIEVING RESULT INTO C
+    for i = 1 : size(S2,1)
+        predicted_age = S2{i,5};
+        real_age = S2{i,6};
+        error = abs(predicted_age - real_age);
+        
+        C{kk,1} = vertcat(C{kk,1}, predicted_age);
+        C{kk,2} = vertcat(C{kk,2}, real_age);
+        C{kk,3} = vertcat(C{kk,3}, error);
+    end
+    
+    C{kk,4} = mean(C{kk,3});
+    
 end
  
+%% Mean Age Error CALCULATION
+for kk = 1 : numFolds
+    MAE = MAE + C{kk,4};
+end
 
+MAE = MAE / numFolds;
+
+assignin('base', 'C', C);
+%profile viewer
 end
 
 
--- src/plsr/split.m
+++ src/plsr/split.m
@@ -1,22 +1,25 @@
-function s = split(X, Y, sections)
+function s = split(X, Y, sections, REAL_Y)
 %SPLIT_SECTION Summary of this function goes here
     % ex :
     %   X = features
-    %   Y = ages
+    %   Y = ages used for split
     %   sections = [20 50 70]    
+    %   REAL_Y = Real age of person
     
     nbSections = size(sections, 2);
     nbData = size(Y,1);
     s = cell(nbSections, 5);    
+
 
     %% SECTIONS INIT
     for i = 1 : nbSections+1
         s{i, 3} = [];
         s{i, 4} = [];
         s{i, 5} = 'BETAS';
+        s{i, 6} = [];
         
         if i == 1
-           s{i, 1} = 0;
+           s{i, 1} = -1000;
            s{i, 2} = sections(i);
            
         elseif i <= nbSections
@@ -26,7 +29,7 @@ function s = split(X, Y, sections)
            
         else
             s{i, 1} = sections(i - 1) + 1;
-            s{i, 2} = 100;
+            s{i, 2} = 1000;
         end
     end
       
@@ -35,6 +38,12 @@ function s = split(X, Y, sections)
         
         feature = X(i, :);
         age = Y(i);
+        
+        if  exist('REAL_Y')
+            RY = REAL_Y(i,1);
+        else
+            RY = '';
+        end
         
         % Pour chaque individu on le met dans la bonne tranche
         for j = 1 : nbSections+1
@@ -45,10 +54,12 @@ function s = split(X, Y, sections)
             if age >= mini && age <= maxi
                 s{j, 3} = vertcat(s{j,3}, feature);
                 s{j, 4} = vertcat(s{j,4}, age);
+                s{j, 6} = vertcat(s{j, 6}, RY);
             end
             
         end
     end
     %assignin('base', 'S', s);
+    
 end
 
